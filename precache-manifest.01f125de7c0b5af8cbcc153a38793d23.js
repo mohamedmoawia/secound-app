baa2ed702d84f06cd797a82714622c3599b79fa0
mohamedmoawia <mo3wia94@gmail.com>
2020-06-15 20:16:01 +0200

    Updates

---
+++ precache-manifest.01f125de7c0b5af8cbcc153a38793d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7859647d679c3170b7f0dee132526b6a",
+    "url": "/secound-app/index.html"
+  },
+  {
+    "revision": "b3e964535e3aedde41fa",
+    "url": "/secound-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9756f3e69793df849958",
+    "url": "/secound-app/static/js/2.4d9fead2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/secound-app/static/js/2.4d9fead2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e964535e3aedde41fa",
+    "url": "/secound-app/static/js/main.d92517b3.chunk.js"
+  },
+  {
+    "revision": "892c09582fe061e21881",
+    "url": "/secound-app/static/js/runtime-main.af4637b7.js"
+  }
+]);
